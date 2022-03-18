a52ae84f215ed1cb4e905acc008672df5c888e8f
Matteo C <hello@micorksen.xyz>
2022-03-18 19:23:17 +0100

    Create os-functions.h

---
+++ src/os-functions.h
@@ -0,0 +1,15 @@
+/******************************************************
+ * Copyright (c) 2022 Micorksen.
+ * 
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ ******************************************************/
+
+#ifned OS_FUNCTIONS_H
+#define OS_FUNCTIONS_H
+
+void check_permission();
+void get_adapter();
+
+#endif
