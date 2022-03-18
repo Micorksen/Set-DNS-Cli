6e46a56a610ea97b27b02829d17833b9135bb714
Matteo C <hello@micorksen.xyz>
2022-03-18 19:19:00 +0100

    Update linux.cpp

--- src/os/linux.cpp
+++ src/os/linux.cpp
@@ -5,3 +5,12 @@
  * license that can be found in the LICENSE file or at
  * https://opensource.org/licenses/MIT.
  ******************************************************/
+
+using namespace std;
+void check_permission(){
+	cout << "Checked permissions." << endl;
+};
+
+void get_adapter(){
+	cout << "Adapter is : eth0." << endl;
+};
